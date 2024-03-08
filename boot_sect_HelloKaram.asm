b47e21eac3e9665f909b3b252217ce1d5ab9e7ac
KaramAbuGhaboush <146943099+KaramAbuGhaboush@users.noreply.github.com>
2024-03-08 18:40:26 +0200

    add

--- boot_sect_HelloKaram.asm
+++ boot_sect_HelloKaram.asm
@@ -1,4 +1,7 @@
-mov ah, 0x0e ; tty mode
+;raise interrupt 0x10 which is a general interrupt for video services.
+;-------------------------------------------------------------------------------------------
+mov ah, 0x0e ; tty mode | 0x0e on ah tells the video interrupt that the 
+             ; actual function we want to run is to 'write the contents of al in tty mode'.
 mov al, 'H'
 int 0x10
 mov al, 'e'
@@ -21,5 +24,6 @@ int 0x10
 
 jmp $ ; Jump to the current address (Infinite loop)
 
+; padding and magic number
 times 510 - ($-$$) db 0
 dw 0xaa55
